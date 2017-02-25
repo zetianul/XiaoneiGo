ca3977e02f1f0a4743b744e4c7f3b760f7d6fa14
fremon <840299234@qq.com>
2017-02-25 16:50:58 +0800

    first

---
+++ src/main/java/com/seu/fff/dao/CityMapper.java
@@ -0,0 +1,36 @@
+package com.seu.fff.dao;
+
+import org.apache.ibatis.annotations.Param;
+import com.seu.fff.pojo.City;
+
+import java.util.List;
+
+/**
+ * 城市 DAO 接口类
+ *
+ * Created by bysocket on 07/02/2017.
+ */
+public interface CityMapper {
+
+    /**
+     * 获取城市信息列表
+     *
+     * @return
+     */
+
+    List<City> findAllCity();
+
+    /**
+     * 根据城市 ID，获取城市信息
+     *
+     * @param id
+     * @return
+     */
+    City findById(@Param("id") Long id);
+
+    Long saveCity(City city);
+
+    Long updateCity(City city);
+
+    Long deleteCity(Long id);
+}
---
+++ src/main/java/com/seu/fff/serviceimpl/CityServiceImpl.java
@@ -0,0 +1,45 @@
+package com.seu.fff.serviceimpl;
+
+import com.seu.fff.dao.CityMapper;
+import com.seu.fff.pojo.City;
+import com.seu.fff.service.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 城市业务逻辑实现类
+ *
+ * Created by bysocket on 07/02/2017.
+ */
+@Service
+public class CityServiceImpl implements CityService {
+
+    @Autowired
+    private CityMapper cityMapper;
+
+    public List<City> findAllCity(){
+        return cityMapper.findAllCity();
+    }
+
+    public City findCityById(Long id) {
+        return cityMapper.findById(id);
+    }
+
+    @Override
+    public Long saveCity(City city) {
+        return cityMapper.saveCity(city);
+    }
+
+    @Override
+    public Long updateCity(City city) {
+        return cityMapper.updateCity(city);
+    }
+
+    @Override
+    public Long deleteCity(Long id) {
+        return cityMapper.deleteCity(id);
+    }
+
+}
